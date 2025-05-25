014274d79d5654c82a0652207efb966c51eac4ce
Chicago-tr <programmingclouds@gmail.com>
2025-05-25 02:57:25 -0500

    Update Api_to_csv.py

--- Api_to_csv.py
+++ Api_to_csv.py
@@ -10,13 +10,14 @@ def dailydata_csv(symbol_list, key):
     data_type = "csv" # for specifying csv output
 
     for symbol in symbols_list:
+        
         url = f"https://www.alphavantage.co/query?function={function}&symbol={symbol}&apikey={key}&outputsize={output_size}&datatype={data_type}"
-
         response = requests.get(url)
 
         if response.status_code == 200:
             # Successful response code
             csv_data = response.text
+            
             with open(f"{symbol}_daily.csv", "w") as file:
                 file.write(csv_data)
                 print(f"CSV data saved to {symbol}_daily.csv")
@@ -30,8 +31,6 @@ def dailydata_csv(symbol_list, key):
 if __name__ == '__main__':
 
     symbols_list = ['List of ticker symbols to obtain data for']
-
     key= "Alphavantage Api Key"
-
 
     dailydata_csv(symbols_list, key)
